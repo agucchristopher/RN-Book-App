5207dd5fefcdbcd7bb567bd3aaa42238b9bdefdb
Agu Christopher <aguchris740@gmail.com>
2023-10-08 16:21:14 +0100

    Started the project

---
+++ app/bookinfo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Bookinfo = () => {
+  return (
+    <View>
+      <Text>Bookinfo</Text>
+    </View>
+  );
+};
+
+export default Bookinfo;
+
+const styles = StyleSheet.create({});
